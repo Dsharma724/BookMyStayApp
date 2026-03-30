4cf6beb6a3e337171a7976147f11aa083fb0b365
Dsharma724 <dhairaysharma7@gmail.com>
2026-03-30 09:35:56 +0530

    Use Case 9: Error Handling & Validation

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,24 +1,49 @@
-public class BookMyStayApp {
+import java.util.Scanner;
+
+public class BookMyStayApp{
 
     public static void main(String[] args) {
 
-        System.out.println("Booking History and Reporting");
+        System.out.println("Booking Validation");
 
-        // Create booking history
-        BookingHistory history = new BookingHistory();
+        Scanner scanner = new Scanner(System.in);
 
-        // Assume these were confirmed (from UC6)
-        Reservation r1 = new Reservation("Abhi", "Single");
-        Reservation r2 = new Reservation("Subha", "Double");
-        Reservation r3 = new Reservation("Vanmathi", "Suite");
+        // Initialize components
+        RoomInventory inventory = new RoomInventory();
+        ReservationValidator validator = new ReservationValidator();
+        BookingRequestQueue queue = new BookingRequestQueue();
 
-        // Add to history
-        history.addReservation(r1);
-        history.addReservation(r2);
-        history.addReservation(r3);
+        // Setup inventory (important!)
+        inventory.addRooms("Single", 2);
+        inventory.addRooms("Double", 2);
+        inventory.addRooms("Suite", 1);
 
-        // Generate report
-        BookingReportService reportService = new BookingReportService();
-        reportService.generateReport(history);
+        try {
+            // Take input
+            System.out.print("Enter guest name: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Enter room type (Single/Double/Suite): ");
+            String roomType = scanner.nextLine();
+
+            // VALIDATION STEP
+            validator.validate(name, roomType, inventory);
+
+            // If valid → create reservation
+            Reservation reservation = new Reservation(name, roomType);
+
+            // Add to queue
+            queue.addRequest(reservation);
+
+            System.out.println("Booking request accepted.");
+
+        } catch (InvalidBookingException e) {
+
+            // Graceful failure
+            System.out.println("Booking failed: " + e.getMessage());
+
+        } finally {
+            scanner.close();
+        }
     }
 }
